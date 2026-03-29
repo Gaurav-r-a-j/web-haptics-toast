ede70a334fa929da57a70801367c661c83600e12
Gaurav Raj <gk5271@srmist.edu.in>
2026-03-29 14:01:52 +0530

    fixed build errors

--- src/assets.tsx
+++ src/assets.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react';
+import React, { JSX } from 'react';
 import type { ToastTypes } from './types';
 
 export const getAsset = (type: ToastTypes): JSX.Element | null => {
